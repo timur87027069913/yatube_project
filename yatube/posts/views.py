5af88d4cb6c395c0a0baba983dabb3fba87c3d4f
Тимур Мажиков <timur87027069913@gmail.com>
2023-02-16 00:20:54 +0600

    views

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком Сообщения
+def group_posts(request):
+    return HttpResponse('Сообщения')
+
+
+# Страница с информацией об одном сорте мороженого;
+# view-функция принимает параметр pk из path()
+
+#def ice_cream_detail(request, pk):
+#    return HttpResponse(f'Мороженое номер {pk}') 
