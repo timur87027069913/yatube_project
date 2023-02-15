18ebc1035346429bf7ac745e976b9bf80b485394
Тимур Мажиков <timur87027069913@gmail.com>
2023-02-16 00:31:14 +0600

    views

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
-def index(request):    
+def index(request):
     return HttpResponse('Главная страница')
 
 
