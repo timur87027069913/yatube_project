b85ddd62f468894993e6f60a7a6b1f5f566da66a
Тимур Мажиков <timur87027069913@gmail.com>
2023-02-17 20:10:36 +0600

    setting

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse('Ты <i>не можешь</i> получить правильные <b>ответы</b>,<br> '
-        'если у тебя нет правильных <s>вопросов</s> запросов.')
+    template = 'index.html'
+    return render(request, template)
 
 
-# Страница со списком Сообщения
 def group_posts(request):
     return HttpResponse('Сообщения')
-
-
-# Страница с информацией об одном сорте мороженого;
-# view-функция принимает параметр pk из path()
-
-#def ice_cream_detail(request, pk):
-#    return HttpResponse(f'Мороженое номер {pk}') 
