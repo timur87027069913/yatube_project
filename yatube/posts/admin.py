a3b7e6196afae4fca9b74792d18f661181e80e87
Тимур Мажиков <timur87027069913@gmail.com>
2023-02-21 22:30:16 +0600

    views

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-# Из модуля models импортируем модель Post
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author') 
+    search_fields = ('text',) 
+    list_filter = ('pub_date',)
+    # Это свойство сработает для всех колонок: где пусто — там будет эта строка 
+    empty_value_display = '-пусто-'  
 
-class AuthorAdmin(admin.ModelAdmin):
-    date_hierarchy = 'pub_date'
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+
+
+admin.site.register(Post, PostAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 
 def index(request):
-    template = 'index.html'
-    return render(request, template)
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request):
