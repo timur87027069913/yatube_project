c1f392a8e812d54f5049805388323ff753c1b720
Тимур Мажиков <timur87027069913@gmail.com>
2023-02-21 21:35:59 +0600

    admin

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 # Из модуля models импортируем модель Post
 from .models import Post
 
+
 admin.site.register(Post)
+
+class AuthorAdmin(admin.ModelAdmin):
+    date_hierarchy = 'pub_date'
